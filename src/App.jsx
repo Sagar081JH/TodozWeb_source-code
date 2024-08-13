b67f034ffc1e21014b2a85684d3cd1f1ad490b1d
Sagar081JH <sghumare11@gmail.com>
2024-08-13 12:21:26 +0530

    minor ui enhancements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import TodosWithSpring from "./Components/TodosWithSpring";
-import About from "./Components/About";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TodosPureReact from "./Components/TodosPureReact";
 
 function App() {
